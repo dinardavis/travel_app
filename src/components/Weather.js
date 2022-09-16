06208ea7f49b3c421c54433bbde3f191e9e596fb
Dinar Davis <dinardavis@gmail.com>
2022-09-16 11:39:00 +0300

    Add weather icon functionality

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -15,26 +15,46 @@ export default function Weather(props) {
 
   function toggleUnits() {
     if(tempUnits === "imperial") {
-      console.log("it's metric now")
       return setTempUnits("metric")
     } else {
-      console.log("it's imperial now")
       return setTempUnits("imperial")
     }
   }
 
-  console.log(weatherInfo)
-  
+  console.log(weatherInfo.weather[0].main)
+
+  const selectWeatherIcon = (data) => {
+      let weatherIcon = ""
+      if(data === "Clouds") {
+        weatherIcon = "clouds.png";
+      } else if(data === "Drizzle" || data === "Rain") {
+        weatherIcon = "rain.png";
+      } else if(data === "Thunderstorm") {
+        weatherIcon = "lightening.png";
+      } else if(data === "Snow") {
+        weatherIcon = "snow.png";
+      } else if(data === "Mist" || data === "Haze" || data === "Fog") {
+        weatherIcon = "fog.png";
+      } else if(data === "Smoke" || data === "Dust" || data === "Sand" || data === "Ash" || data === "Squall") {
+        weatherIcon = "wind.png";
+      } else if(data === "Tornado") {
+        weatherIcon = "tornado.png"
+      } else {
+        weatherIcon = "sun.png";
+      }
+      return weatherIcon
+  }
+
   return (
     <>
       {!weatherInfo ? 
         <p className="loading">Loading...</p> :
         <div className="weather-container">
-          <img className="weather--icon" src="/imgs/sunny.png" alt="sunny day icon"/>
+          <img className="weather--icon" src={`/imgs/weather/${selectWeatherIcon(weatherInfo.weather[0].main)}`} alt={weatherInfo.weather[0].description} />
           <p className="weather--location">{weatherInfo.name}, {weatherInfo.sys.country}</p>
-          <p className="weather--temp">{Math.round(weatherInfo.main.temp)} &deg;F</p>
+          <p className="weather--temp">{Math.round(weatherInfo.main.temp)} {tempUnits === "imperial" ? "°F" : "°C" }</p>
           <div className="weather--desc">{weatherInfo.weather[0].main} </div>
-          <p className="weather--feels">Feels Like: {Math.round(weatherInfo.main.feels_like)} &deg;F</p>
+          <p className="weather--feels">Feels Like: {Math.round(weatherInfo.main.feels_like)} {tempUnits === "imperial" ? "°F" : "°C" }</p>
        
           {/* <p className="weather--time">{weatherInfo.dt}</p> */}
         
@@ -43,7 +63,7 @@ export default function Weather(props) {
               <p className="weather--high">High: {Math.round(weatherInfo.main.temp_max)} &deg;F</p> •
               <p className="weather--low">Low: {Math.round(weatherInfo.main.temp_min)} &deg;F</p>
             </div>
-            <div onClick={toggleUnits}>click me!</div>
+            <div className="toggle-temp-units" onClick={toggleUnits}>°F / °C</div>
             {/* <div className="temp-toggle">
               <p>&deg;F</p>
               <label className="switch" onClick={toggleUnits}>
