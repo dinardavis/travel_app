75bb25bcc2914f81c0094ea5d05e6beb9df4b4aa
Dinar Davis <dinardavis@gmail.com>
2022-10-27 19:46:49 +0700

    Add widgets module to sidbar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,17 @@
 import React from "react";
 import Login from "./Login";
+import { BsPinMap } from "react-icons/bs"
 
 export default function Sidebar() {
 
   return (
-    <section className="sidebar-container">
+    <section 
+    className="sidebar-container">
+      <div className="widgets-container">
+        <h1 className="sidebar-header">Widgets</h1>
+        <div className="widgets widget-map"><BsPinMap className="widget-icon" /><p>Map</p></div>
+      </div>
+    
       <Login />
     </section>
   )
