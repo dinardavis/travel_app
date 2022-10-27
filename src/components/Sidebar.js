a925637bc5ac0d6f21b5b3793d2be47d880efbdc
Dinar Davis <dinardavis@gmail.com>
2022-10-27 20:02:14 +0700

    Add addtional widget modules & revise styling

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Login from "./Login";
 import { BsPinMap } from "react-icons/bs"
+import { BsPlusSquareFill } from "react-icons/bs"
+import { BsQuestionDiamond } from "react-icons/bs"
 
 export default function Sidebar() {
 
@@ -9,7 +11,9 @@ export default function Sidebar() {
     className="sidebar-container">
       <div className="widgets-container">
         <h1 className="sidebar-header">Widgets</h1>
-        <div className="widgets widget-map"><BsPinMap className="widget-icon" /><p>Map</p></div>
+        <div className="widgets widget-map"><BsPinMap className="widget-icon" /><p>Map</p><BsPlusSquareFill className="widget-add-icon"/></div>
+
+        <div className="widgets widget-advisory"><BsQuestionDiamond className="widget-icon advisory" /><p>Advisories</p><BsPlusSquareFill className="widget-add-icon"/></div>
       </div>
     
       <Login />
