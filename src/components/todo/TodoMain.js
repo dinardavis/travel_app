3690cfcddf4fe38672aa936ce37fe0325deb047d
Dinar Davis <dinardavis@gmail.com>
2022-10-28 19:46:00 +0700

    Add functionality to store todo items in localStorage

--- src/components/todo/TodoMain.js
+++ src/components/todo/TodoMain.js
@@ -3,19 +3,13 @@ import Form from './Form';
 import List from './List';
 
 export default function TodoMain() {
-  const [todoList, setTodoList] = React.useState([
-    {
-    text:"Renew Passport",
-    key:"1",
-    id:"1",
-    },
-    {
-      text:"Setup International Phone Plan",
-      key:"2",
-      id:"2",
-      },
-  ])
+  const [todoList, setTodoList] = React.useState(JSON.parse(localStorage.getItem("savedTodos")) || []
+  )
   const [userInput, setUserInput] = React.useState("")
+
+  React.useEffect(() => {
+    localStorage.setItem("savedTodos", JSON.stringify(todoList))
+  }, [todoList])
 
   return (
     <section className='todo-main-container'>
