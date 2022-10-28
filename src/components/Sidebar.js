6100e2fd41a69a5da47ac96a1ccfe3c7793f7baf
Dinar Davis <dinardavis@gmail.com>
2022-10-29 01:11:44 +0700

    Add additional widget ideas & modify styling

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Login from "./Login";
 import { BsPinMap } from "react-icons/bs"
-import { BsPlusSquareFill } from "react-icons/bs"
 import { BsQuestionDiamond } from "react-icons/bs"
+import { BsPiggyBank } from "react-icons/bs"
+import { BsClock } from "react-icons/bs"
 
 export default function Sidebar() {
 
@@ -11,9 +12,29 @@ export default function Sidebar() {
     className="sidebar-container">
       <div className="widgets-container">
         <h1 className="sidebar-header">Widgets</h1>
-        <div className="widgets widget-map"><BsPinMap className="widget-icon" /><p>Map</p><BsPlusSquareFill className="widget-add-icon"/></div>
+        <div className="widgets widget-map">
+          <BsPinMap className="widget-icon" />
+          <p className="widget-text">Map</p>
+          <div className="widget-add-btn">+</div>
+        </div>
 
-        <div className="widgets widget-advisory"><BsQuestionDiamond className="widget-icon advisory" /><p>Advisories</p><BsPlusSquareFill className="widget-add-icon"/></div>
+        <div className="widgets widget-advisory">
+          <BsQuestionDiamond className="widget-icon advisory" />
+          <p className="widget-text">Travel Advisories</p>
+          <div className="widget-add-btn">+</div>
+        </div>
+
+        <div className="widgets widget-budget">
+          <BsPiggyBank className="widget-icon advisory" />
+          <p className="widget-text">Budget</p>
+          <div className="widget-add-btn">+</div>
+        </div>
+
+        <div className="widgets widget-pto">
+          <BsClock className="widget-icon advisory" />
+          <p className="widget-text">PTO Tracker</p>
+          <div className="widget-add-btn">+</div>
+        </div>
       </div>
     
       <Login />
