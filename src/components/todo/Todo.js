fc46ea61c69f0b501492e19295f04b6902bd5a0b
Dinar Davis <dinardavis@gmail.com>
2022-10-31 13:14:15 +0700

    Add functionality to mark todo items completed with line-through style

--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -5,7 +5,17 @@ import { BsCheck2Square } from "react-icons/bs"
 
 export default function Todo(props) {
 
-  console.log(props.completed)
+  const styles = {
+    textDecoration: props.completed ? "line-through" : "none"
+  }
+
+  function markCompleted(id){
+    props.setTodoList(prevList => prevList.map(item => {
+      return item.id === props.id ?
+      {...item, completed: !item.completed} :
+      item
+    }))
+  }
 
   function deleteTodoItem() {
     props.setTodoList(props.todoList.filter(item => {
@@ -14,11 +24,12 @@ export default function Todo(props) {
   }
   
   return (
-    <div className='list-item'>
+    <div className='list-item' style={styles}>
       <div className='category-marker'><BsFillCheckCircleFill className='category-checkmark'/></div>
       {props.text}
       <BsCheck2Square 
         className='todo-icon check-icon'
+        onClick={() => markCompleted(props.id)}
       />
       <BsTrash 
         className='todo-icon trash-icon'
