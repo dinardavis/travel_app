65285b5fd5a5eac52f65c2819885885ab534f1d4
Dinar Davis <dinardavis@gmail.com>
2022-11-07 11:05:17 +0700

    Connect google.com search capability to navbar search input

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,13 +2,11 @@ import React from 'react'
 
 export default function Search() {
 
+  
   return (
-    <form className='google-search'>
-      <input
-        placeholder='Search the Local Sights'
-      >
-        
-      </input>
+    <form className="google-search" action="https://www.google.com/search" method="get" name="searchform" target="_blank">
+      <input name="sitesearch" type="hidden" />
+      <input autoComplete="on" name="q" placeholder="Must see in..." type="text" />
     </form>
   )
 }
